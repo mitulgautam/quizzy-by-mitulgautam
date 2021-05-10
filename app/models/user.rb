94406a581d732bc27ffa08a3c53fe41c1c84986d
Mitul Gautam <mitulgautam@outlook.com>
2021-05-10 19:19:06 +0530

    User test case completed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   validates_presence_of :email, :first_name, :last_name
   validates_length_of :first_name, minimum: 3, maximum: 50
   validates_length_of :last_name, minimum: 3, maximum: 50
+  validates_uniqueness_of :email, :case_sensitive => false
+  validates :email, email: {mode: :strict, require_fqdn: true}
+  before_save :downcase_email
+
+  def downcase_email
+    self.email.downcase!
+  end
 end
