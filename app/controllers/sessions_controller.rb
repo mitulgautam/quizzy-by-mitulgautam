1304a7d3db7699abddef29b4c65e92cd5bc2578b
Mitul Gautam <mitulgautam@outlook.com>
2021-05-12 01:47:49 +0530

    Login API and Session.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: {user: user, notice: "User logged in sucessfully."}, status: :ok
+    else
+      render json: {error: ["Invalid email/password. Please check and try again!"]}, status: :unprocessable_entity
+    end
+  end
+
+  def current_user
+    if @current_user
+      render json: @current_user, status: :ok
+    else
+      render json: {errors: ["No user logged in."], notice: "Unauthorised"}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    render json: {message: "User logged out successfully."}
+  end
+end
