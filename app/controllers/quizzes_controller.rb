91855c2d14dc2347093e8534e5ca9a5d694f07c0
Mitul Gautam <mitulgautam@outlook.com>
2021-05-14 01:17:12 +0530

    Added CRUD Methods in Quiz Controller.

---
+++ app/controllers/quizzes_controller.rb
@@ -0,0 +1,43 @@
+class QuizzesController < ApplicationController
+  before_action :set_quiz, only: [:update, :destroy, :show]
+
+  def create
+    @quiz = @current_user.quizzes.build(permitted_params)
+    if @quiz.save
+      render json: { notice: "Quiz added successfully." }, status: :ok
+    else
+      render json: { errors: @quiz.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @quizzes = @current_user.quizzes
+    render json: @quizzes, status: :ok, each_serializer: QuizSerializer
+  end
+
+  def show
+    render json: @quiz, status: :ok
+  end
+
+  def destroy
+      @quiz.destroy
+      render json: {notice: "Quiz has been deleted sucessfully"}, status: :ok
+  end
+
+  def update
+    @quiz.update(permitted_params)
+    render json: {notice: "Quiz has been updated sucessfully"}, status: :ok
+  end
+
+  private
+    def permitted_params
+      params.require(:quiz).permit(:name)
+    end
+
+    def set_quiz
+      @quiz = Quiz.find_by(id: params[:id])
+      if @quiz.nil?
+        render json: {errors: "Quiz id is not valid."}, status: :unprocessable_entity
+      end
+    end
+end
