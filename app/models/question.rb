6d882f9d9c9b9585d624ac568d0debe2363db8c1
Mitul Gautam <mitulgautam@outlook.com>
2021-05-18 13:02:19 +0530

    Added unit test for question and option model.

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,17 @@ class Question < ApplicationRecord
   belongs_to :quiz
   has_many :options, dependent: :destroy
   accepts_nested_attributes_for :options, allow_destroy: true
+  validate :validates_options
+  validates :name, presence: true
+  validates :options, presence: true
+  validates :correct_option, presence: true
+  
+  private
+  def validates_options
+    if self.options.length > 4
+      errors.add(:base, "Max 4 options can be added to a question.")
+    elsif self.options.length < 2
+      errors.add(:base, "Min 2 options are required to create a question")
+    end
+  end
 end
